91f100d89e88edc2f349b7c79bb89c7275c052bd
Brian McMahan <braingineer@users.noreply.github.com>
2016-02-28 18:37:07 -0500

    Update bots.py

--- wormwars/bots.py
+++ wormwars/bots.py
@@ -12,6 +12,7 @@ class GenericWormBot:
         self.body_parts = [{'x':x, 'y':y}]
         self.FAILED = False
         self.FAILURE_REASON = "Hasn't Failed"
+        self.last_history = []
 
 
     @classmethod
@@ -20,7 +21,9 @@ class GenericWormBot:
         return cls(bot_id, starting_position)
 
     def act(self, game, bots):
-        move_x, move_y = self.think(game, bots)
+        t = self.think(game, bots)
+        print(t)
+        move_x, move_y = t
         new_head_x = self.body_parts[consts.HEAD]['x'] + move_x
         new_head_y = self.body_parts[consts.HEAD]['y'] + move_y
 
@@ -114,50 +117,105 @@ class AwesomeBot(GenericWormBot):
                 all_moves.append((move_dist, new_coord, move_value))
         return all_moves
 
-    def think(self, game, bots):
+    def apply_moves_bare(self, xy, game):
+        all_moves = []
+        for move_name, move_value in consts.MOVES.items():
+            new_coord = self.apply_move(move_value, xy)
+            if not self.bad_move(new_coord, game):
+                all_moves.append((new_coord, move_value))
+        return all_moves
 
-        foodxy = (game.food['x'], game.food['y'])
+    def think(self, game, bots):
+        n = len(self.last_history)
+        # this version makes it crazy
+        #if n > 0 and n % 20 != 0:
+        if n > 0:
+            return self.last_history.pop()
         head = self.body_parts[consts.HEAD]
         curxy = (head['x'], head['y'])
-        starting_point = (0,curxy, None)
+        move = astar(game, self)
+        return move
 
-        frontier = PriorityQueue()
-        frontier.push(starting_point)
-        came_from = dict()
-        graveyard = set()
-        best_move = None
+def astar(game, bot):
+    foodxy = (game.food['x'], game.food['y'])
+    head = bot.body_parts[consts.HEAD]
+    curxy = (head['x'], head['y'])
+    starting_point = (0, 0,curxy, None, 0)
 
-        while frontier.not_empty():
-            move_dist, next_move, move_value = frontier.pop()
-            if next_move == foodxy:
-                best_move = (move_dist, next_move, move_value)
-                break
-            moves = self.apply_moves(next_move, foodxy, game)
-            for move in moves:
-                move_coord = move[1]
-                if move_coord not in graveyard:
-                    came_from[move] = (move_dist, next_move, move_value)
-                    frontier.push(move)
-                    graveyard.add(move_coord)
-
-        if best_move is None:
-            print("found nothing.. food unreachable.. but can we just not die?")
-            for move, origin in came_from.items():
-                if origin == starting_point:
-                    move_value = move[2]
-                    return move_value
-            print("I think we're about to die")
-            return list(consts.MOVES.values())[0]
-
-        self.last_history = []
-        justincase = 0
-        self.last_history.append(best_move)
-        while came_from[best_move][0] > 0 and justincase < 10**4:
-            best_move = came_from[best_move]
-            self.last_history.append(best_move)
-            justincase += 1
-
-        move_dist, next_move, move_value = best_move
-        return move_value
+    frontier = PriorityQueue()
+    frontier.push(starting_point)
+    came_from = dict()
+    graveyard = set()
+    best_move = None
 
 
+    while frontier.not_empty():
+        astarval, move_dist, next_move, move_value, move_num = frontier.pop()
+        if next_move == foodxy:
+            best_move = (astarval, move_dist, next_move, move_value, move_num)
+            break
+        moves = bot.apply_moves(next_move, foodxy, game)
+        for move in moves:
+            move = move + (move_num+1, )
+            move_coord = move[1]
+            next_astarval = move[0] + move[-1]
+            move = (next_astarval, ) + move
+            if move_coord not in graveyard:
+                came_from[move] = (astarval, move_dist, next_move, move_value, move_num)
+                frontier.push(move)
+                graveyard.add(move_coord)
+
+    if best_move is None:
+        return astar_dontdie(game, bot)
+
+
+    bot.last_version = "normal"
+    #astarval, move_dist, next_move, move_value, move_num = \
+    get_best(bot, best_move, came_from, pos=3)
+    return bot.last_history.pop()
+
+def astar_dontdie(game, bot):
+    print('in dont die')
+    head = bot.body_parts[consts.HEAD]
+    curxy = (head['x'], head['y'])
+    butt = bot.body_parts[-1]
+    buttxy = (butt['x'], butt['y'])
+    starting_point = (0, curxy, None)
+
+    frontier = PriorityQueue()
+    frontier.push(starting_point)
+    came_from = dict()
+    graveyard = set()
+    best_move = None
+
+
+    while frontier.not_empty():
+        move_num, next_move, move_value = frontier.pop()
+        moves = bot.apply_moves_bare(next_move, game)
+        for move in moves:
+            move = (move_num-1, move[0], move[1])
+            move_coord = move[1]
+            if move_coord not in graveyard:
+                came_from[move] = (move_num, next_move, move_value)
+                frontier.push(move)
+                graveyard.add(move_coord)
+    if best_move == None:
+        print("this is none")
+        if len(came_from) == 0:
+            print('we are screwed and dead')
+            return list(consts.MOVES.values())[1]
+        best_move = min(came_from.items(), key=lambda x: x[0][0])[1]
+
+    bot.last_version = "dontdie"
+    get_best(bot, best_move, came_from, -1, 2)
+    return bot.last_history.pop()
+
+def get_best(bot, best_move, came_from, mod=1, pos=1):
+    bot.last_history = []
+    justincase = 0
+    print(pos)
+    bot.last_history.append(best_move[pos])
+    while mod*came_from[best_move][0] > 0 and justincase < 10**4:
+        best_move = came_from[best_move]
+        justincase += 1
+        bot.last_history.append(best_move[pos])
